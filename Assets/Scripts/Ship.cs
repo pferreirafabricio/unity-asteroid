245382edad62c160c5bdd81a324564b7a480462e
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-10-31 12:57:21 -0300

    initial commit

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private int _speed;
+
+    public GameObject shot;
+
+    void Start()
+    {
+        this._speed = 10;
+    }
+
+    void Update()
+    {
+        this.movimentX();
+        this.movimentY();
+
+        if (Input.GetKeyDown("space"))
+        {
+            this.instantiateShot();
+        }
+    }
+
+    private void movimentX()
+    {
+        float xAxis = ((Input.GetAxis("Horizontal") * _speed) * Time.deltaTime);
+        transform.Translate(xAxis, 0, 0);
+    }
+
+    private void movimentY()
+    {
+        float yAxis = ((Input.GetAxis("Vertical") * _speed) * Time.deltaTime);
+        transform.Translate(0, yAxis, 0);
+    }
+
+    private void instantiateShot()
+    {
+        Instantiate(shot, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    private int _speed = 6;
+
+    void Start()
+    {
+        Rigidbody2D _rigidBody = GetComponent<Rigidbody2D>();
+        _rigidBody.velocity = new Vector2(0, _speed);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
